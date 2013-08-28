58cd3c3160d1354589a3e58b2f9b8348107e45e0
Felipe Torres <felipe.gtorres@gmail.com>
2013-08-28 16:24:37 -0300

    Usando contextActionBar em vez de contextMenu

--- BolsaDeValores/src/com/example/bolsadevalores/activity/StockActivity.java
+++ BolsaDeValores/src/com/example/bolsadevalores/activity/StockActivity.java
@@ -1,15 +1,11 @@
 package com.example.bolsadevalores.activity;
 
-import java.util.List;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
+import android.support.v7.view.ActionMode.Callback;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -20,16 +16,14 @@ import com.example.bolsadevalores.R;
 import com.example.bolsadevalores.helper.ErrorDialog;
 import com.example.bolsadevalores.helper.ErrorHandler;
 import com.example.bolsadevalores.helper.OptionsMenuDelegator;
-import com.example.bolsadevalores.helper.SharedPreferencesAccessor;
-import com.example.bolsadevalores.json.JSONListResponseObject;
-import com.example.bolsadevalores.json.JSONSingleResponseObject;
+import com.example.bolsadevalores.menu.CustomActionMode;
 import com.example.bolsadevalores.model.Stock;
-import com.example.bolsadevalores.task.StockTask;
 
 public class StockActivity extends ActionBarActivity implements ErrorHandler {
 
 	private Stock selected;
 	private GridView grid;
+	private CustomActionMode customActionMode;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +36,10 @@ public class StockActivity extends ActionBarActivity implements ErrorHandler {
 		super.onResume();
 
 		grid = (GridView) findViewById(R.id.gridView);
-
-		loadStocksTo(grid);
-
-		registerForContextMenu(grid);
-
+		customActionMode = new CustomActionMode(this, grid, this);
+		
+		customActionMode.loadStocks();
+		
 		grid.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
@@ -67,29 +60,18 @@ public class StockActivity extends ActionBarActivity implements ErrorHandler {
 			@Override
 			public boolean onItemLongClick(AdapterView<?> adapter, View view,
 					int position, long id) {
-
+				
 				selected = (Stock) adapter.getItemAtPosition(position);
-				return false;
+				
+				Callback mode = customActionMode.withSelected(selected).build();
+				StockActivity.this.startSupportActionMode(mode);
+
+				return true;
 			}
 		});
 
 	}
-
-	private void loadStocksTo(GridView grid) {
-		SharedPreferencesAccessor accessor = new SharedPreferencesAccessor(this);
-
-		List<String> bookmarkedStocks = accessor.retrieveBookmarkedStocks();
-		String[] stocks = (String[]) bookmarkedStocks.toArray();
-
-		if (bookmarkedStocks.size() == 2) {
-			new StockTask(this, grid, JSONSingleResponseObject.class, this)
-					.execute(stocks);
-		} else if (bookmarkedStocks.size() > 2) {
-			new StockTask(this, grid, JSONListResponseObject.class, this)
-					.execute(stocks);
-		}
-	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.stock, menu);
@@ -102,23 +84,6 @@ public class StockActivity extends ActionBarActivity implements ErrorHandler {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		return new OptionsMenuDelegator(this).select(item);
-	}
-
-	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v,
-			ContextMenuInfo menuInfo) {
-		MenuItem remove = menu.add("Remover");
-
-		remove.setOnMenuItemClickListener(new OnMenuItemClickListener() {
-
-			@Override
-			public boolean onMenuItemClick(MenuItem item) {
-				new SharedPreferencesAccessor(StockActivity.this)
-						.removeFromBookmark(selected);
-				loadStocksTo(grid);
-				return false;
-			}
-		});
 	}
 
 	@Override
---
+++ BolsaDeValores/src/com/example/bolsadevalores/menu/CustomActionMode.java
@@ -0,0 +1,87 @@
+package com.example.bolsadevalores.menu;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.support.v7.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.GridView;
+
+import com.example.bolsadevalores.R;
+import com.example.bolsadevalores.helper.ErrorHandler;
+import com.example.bolsadevalores.helper.SharedPreferencesAccessor;
+import com.example.bolsadevalores.json.JSONListResponseObject;
+import com.example.bolsadevalores.json.JSONSingleResponseObject;
+import com.example.bolsadevalores.model.Stock;
+import com.example.bolsadevalores.task.StockTask;
+
+public class CustomActionMode {
+	
+	private Activity activity;
+	private Stock selected;
+	private GridView grid;
+	private ErrorHandler handler;
+
+	public CustomActionMode(Activity activity, GridView grid, ErrorHandler handler) {
+		this.activity = activity;
+		this.grid = grid;
+		this.handler = handler;
+	}
+	
+	public CustomActionMode withSelected(Stock selected) {
+		this.selected = selected;
+		return this;
+	}
+
+	public ActionMode.Callback build() {
+		return new ActionMode.Callback() {
+
+			@Override
+			public boolean onPrepareActionMode(ActionMode arg0, Menu arg1) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public void onDestroyActionMode(ActionMode arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+				mode.getMenuInflater().inflate(R.menu.stock_context, menu);
+
+				return true;
+			}
+
+			@Override
+			public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+				if(item.getItemId() == R.id.stock_context_remove) {
+					new SharedPreferencesAccessor(activity).removeFromBookmark(selected);
+					loadStocks();
+					mode.finish();
+					return true;
+				}
+				
+				return false;
+			}
+		};
+	}
+	
+	public void loadStocks() {
+		SharedPreferencesAccessor accessor = new SharedPreferencesAccessor(activity);
+
+		List<String> bookmarkedStocks = accessor.retrieveBookmarkedStocks();
+		String[] stocks = (String[]) bookmarkedStocks.toArray();
+
+		if (bookmarkedStocks.size() == 2) {
+			new StockTask(activity, grid, JSONSingleResponseObject.class, handler)
+					.execute(stocks);
+		} else if (bookmarkedStocks.size() > 2) {
+			new StockTask(activity, grid, JSONListResponseObject.class, handler)
+					.execute(stocks);
+		}
+	}
+}
